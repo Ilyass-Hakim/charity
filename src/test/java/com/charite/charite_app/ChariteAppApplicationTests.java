b5c347c562f43f36a992dbe3e1b46847a2ace50c
Ilyass-Hakim <hakimilyass012@gmail.com>
2026-04-18 20:17:57 +0100

    MISE EN PLACE DU PROJET

---
+++ src/test/java/com/charite/charite_app/ChariteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charite.charite_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChariteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
